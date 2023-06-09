9d7b667dd4854a1bef0946daf83f423d44f647b3
Rakib Hossen <rakibhossen1660@gmail.com>
2023-06-09 07:32:03 +0600

    some change

--- src/pages/Home/Service/Service.jsx
+++ src/pages/Home/Service/Service.jsx
@@ -3,9 +3,10 @@ import ServiceCard from './ServiceCard';
 
 const Service = () => {
     const [services,setServices] = useState([]);
+    const [asc,setAsc] = useState(true);
 
     useEffect(() =>{
-        fetch('http://localhost:5000/services')
+        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'des'}`)
         .then(res => res.json())
         .then(data => setServices(data))
     },[])
@@ -15,6 +16,7 @@ const Service = () => {
             <p className='font-semibold text-orange-700'>Service</p>
             <h2 className='text-4xl font-bold'>Our Service Area</h2>
             <p className='text-xl'>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don't look even slightly believable. </p>
+            <button className='btn' onClick={()=> setAsc(!asc)}>{asc ? 'High to Low' : 'Low to High'}</button>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
                 {
                     services.map(service => <ServiceCard
